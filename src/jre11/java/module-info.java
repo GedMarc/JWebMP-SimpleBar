20157338cc97ca98a5e9d9d08c0c5a90f5941326
GedMarc <ged_marc@hotmail.com>
2020-12-16 22:47:38 +0200

    Turn on Type-Safety in JWebMP, Hibernate 5.4.26, JAnsi 2.2.1

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -1,9 +1,11 @@
+import com.jwebmp.plugins.simplebar.SimpleBarInclusionModule;
 import com.jwebmp.plugins.simplebar.SimpleBarPageConfigurator;
 
 module com.jwebmp.plugins.simplebar {
     requires transitive com.jwebmp.core;
 
     provides com.jwebmp.core.services.IPageConfigurator with SimpleBarPageConfigurator;
-
+    provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleInclusions with SimpleBarInclusionModule;
+    
     opens com.jwebmp.plugins.simplebar to com.google.guice;
 }
---
+++ src/main/java/com/jwebmp/plugins/simplebar/SimpleBarInclusionModule.java
@@ -0,0 +1,18 @@
+package com.jwebmp.plugins.simplebar;
+
+import com.guicedee.guicedinjection.interfaces.IGuiceScanModuleInclusions;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SimpleBarInclusionModule implements IGuiceScanModuleInclusions<SimpleBarInclusionModule>
+{
+	@Override
+	public @NotNull Set<String> includeModules()
+	{
+		Set<String> set = new HashSet<>();
+		set.add("com.jwebmp.plugins.simplebar");
+		return set;
+	}
+}
